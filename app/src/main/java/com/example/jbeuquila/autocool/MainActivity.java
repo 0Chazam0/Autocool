03bc5d57faeb559fb744649635f2f899cc5bd742
0Chazam0 <32390624+0Chazam0@users.noreply.github.com>
2018-03-23 13:27:37 +0100

    Initial commit

---
+++ app/src/main/java/com/example/jbeuquila/autocool/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jbeuquila.autocool;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
